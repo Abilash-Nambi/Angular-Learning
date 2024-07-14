1835f53df78b0a3efd9fd3259fcaded7fe3b5f30
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-07-14 21:20:44 +0530

    user details showing done

--- src/app/users-details-list/users-details-list.component.ts
+++ src/app/users-details-list/users-details-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { User } from '../model';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-users-details-list',
@@ -7,12 +9,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./users-details-list.component.scss'],
 })
 export class UsersDetailsListComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private userServices: UsersService
+  ) {}
   public state: number = 0;
+  public userDetails: User[] = [];
+  public filteredUserData: User[] = [];
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.state = +params['id'];
     });
+    this.userServices.getUsers().subscribe((res) => {
+      (this.userDetails = res), this.filteredData();
+    });
+  }
+
+  filteredData() {
+    if (this.userDetails.length > 0) {
+      this.filteredUserData = this.userDetails.filter(
+        (data) => data.id == this.state
+      );
+      console.log(
+        '🚀 + UsersDetailsListComponent + filteredData +   this.filteredUserData:',
+        this.filteredUserData
+      );
+    }
   }
 }
