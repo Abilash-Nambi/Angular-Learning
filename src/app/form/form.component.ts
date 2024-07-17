945ba3c45ab36dca88240d48bbf73b8126d6c24d
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-07-17 22:12:31 +0530

    form  new directive implemented

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  public userModel = new User('Ajmal', 'abilashn2704@gmail.com', 9895908764);
 }
